389811bfcbc2533067618478938322521dada32d
Daniel Keyes <danieljkeyes@gmail.com>
2022-01-12 00:10:27 -0600

    disable builder selfdestruct farming, to account for sprint nerf

--- src/simpleplayer/Archon.java
+++ src/simpleplayer/Archon.java
@@ -6,8 +6,6 @@ import static simpleplayer.RobotPlayer.*;
 
 public class Archon {
 
-    private static int numSacrificialBuildersBuilt = 0;
-
     private static int[] unitCounts;
 
     public static void runArchon() throws GameActionException {
@@ -70,59 +68,29 @@ public class Archon {
             // some tiles are practically infinite, so we can't mine them all in one game.
             lead = Math.min(lead, 9 * 5 * GameConstants.GAME_MAX_NUMBER_OF_ROUNDS);
             // how many times could we mine from this tile for 45 lead each time over, say, the next 100 turns?
-            // for comparison, a lead tile with 1 lead can be mined for 5 lead once every 20 turns, so 25 per 100 turns
+            // for comparison, a lead tile with 1 lead can be mined for 3 lead once every 50 turns, so 6 per 100 turns
             int timeHorizon = 40;
             double timeToMine = Math.min((lead - 1) / 45., timeHorizon);
             int numWorkers = (int) (timeToMine / 40. * 9);
             effectiveNumNearbyLeadWorkersNeeded += numWorkers;
         }
         // estimate of the number of tiles a single worker can manage. tunable.
-        final int TILES_MANTAINED_BY_WORKER_PER_TURN = 5;
-        effectiveNumNearbyLeadWorkersNeeded += (numNearbyLead + 4) / TILES_MANTAINED_BY_WORKER_PER_TURN;
+        final int TILES_MANTAINED_BY_WORKER_PER_TURN = 10;
+        effectiveNumNearbyLeadWorkersNeeded += (numNearbyLead + TILES_MANTAINED_BY_WORKER_PER_TURN - 1) / TILES_MANTAINED_BY_WORKER_PER_TURN;
 
-        int TARGET_NEARBY_MINERS = 20;
-        int x = locAtStartOfTurn.x;
-        int y = locAtStartOfTurn.y;
-        int widthm1 = rc.getMapWidth() - 1;
-        int heightm1 = rc.getMapHeight() - 1;
-        int visionRadius = Util.sqrt(myType.visionRadiusSquared);
-        int xBelow = Math.max(0, x - visionRadius);
-        int yBelow = Math.max(0, y - visionRadius);
-        int xAbove = Math.min(widthm1, x + visionRadius);
-        int yAbove = Math.min(heightm1, y + visionRadius);
-        double areaNearby = (xAbove - xBelow + 1) * (yAbove - yBelow + 1);
-        // This isn't quite right, because we're slicing circles, but it's a rule of thumb for building miners.
-        double visionDiameter = 2 * visionRadius + 1;
-        double fractionOfArableLand = areaNearby / (visionDiameter * visionDiameter);
-
-        if (numNearbyMiners < TARGET_NEARBY_MINERS * fractionOfArableLand) {
-            // Farm economy
-            if (effectiveNumNearbyLeadWorkersNeeded <= numNearbyMiners && numSacrificialBuildersBuilt < 10) {
-                // need to be creative, seems nothing good is nearby
-                // build a builder to grow a lead mine
-                if (RobotType.BUILDER.buildCostLead <= ourLead) {
-                    for (final Direction d : Directions.RANDOM_DIRECTION_PERMUTATION) {
-                        if (rc.canBuildRobot(RobotType.BUILDER, d)) {
-                            rc.buildRobot(RobotType.BUILDER, d);
-                            numSacrificialBuildersBuilt++;
-                            return;
-                        }
-                    }
-                }
-            } else {
-                // build workers
-                if (RobotType.MINER.buildCostLead <= ourLead) {
-                    for (final Direction d : Directions.RANDOM_DIRECTION_PERMUTATION) {
-                        if (rc.canBuildRobot(RobotType.MINER, d)) {
-                            rc.buildRobot(RobotType.MINER, d);
-                            return;
-                        }
+        if (effectiveNumNearbyLeadWorkersNeeded > numNearbyMiners) {
+            // build workers
+            if (RobotType.MINER.buildCostLead <= ourLead) {
+                for (final Direction d : Directions.RANDOM_DIRECTION_PERMUTATION) {
+                    if (rc.canBuildRobot(RobotType.MINER, d)) {
+                        rc.buildRobot(RobotType.MINER, d);
+                        return;
                     }
                 }
             }
         } else {
             // Build workers if it's necessary to bootstrap, otherwise soldiers
-            if (rc.getRoundNum() < 10 && (unitCounts[RobotType.MINER.ordinal()] < 2 || numNearbyMiners < effectiveNumNearbyLeadWorkersNeeded + 1)) {
+            if (rc.getRoundNum() < 10 && (unitCounts[RobotType.MINER.ordinal()] < 2)) {
                 // build workers
                 if (RobotType.MINER.buildCostLead <= ourLead) {
                     for (final Direction d : Directions.RANDOM_DIRECTION_PERMUTATION) {
